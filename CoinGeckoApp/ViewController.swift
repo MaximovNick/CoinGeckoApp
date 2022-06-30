3739f46028628bcac2dbed6b7163c08942726abc
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-30 13:19:35 +0300

    Initial Commit

---
+++ CoinGeckoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinGeckoApp
+//
+//  Created by Nikolai Maksimov on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
